d5a84c1eb795ae7e8d4b49f6b05e7b4c94d18a87
mikefab <mikefabrikant@gmail.com>
2018-11-01 14:43:03 -0400

    Add auth service

---
+++ auth/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import document from 'global/document';
+import {Provider} from 'react-redux';
+import {hashHistory, Router, Route} from 'react-router';
+import {syncHistoryWithStore} from 'react-router-redux';
+import {render} from 'react-dom';
+import store from './components/magicbox-kepler-demo/client/src/store';
+import registerServiceWorker from './registerServiceWorker';
+
+const history = syncHistoryWithStore(hashHistory, store);
+// const prefix = getAppUrlPrefix();
+// const path = prefix === '' ? '(:id)' : `${prefix}(/:id)`;
+
+const Root = () => (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/(:id)" component={App} />
+      <Route path="/demo/(:id)" component={App} />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+registerServiceWorker();
