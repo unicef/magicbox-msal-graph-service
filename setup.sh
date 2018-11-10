9d5e423cdcaef139a52999f2137eed7268185ac7
mikefab <mikefabrikant@gmail.com>
2018-11-10 17:41:14 -0500

    cp .env-sample

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Adding magicbox-kepler-demo repository
-
+cp ./auth/.env-sample ./auth/.env
 cd auth/src/components || exit echo Sorry, something went wrong. Please try again.
 git clone git@github.com:unicef/magicbox-kepler-demo.git
 cd magicbox-kepler-demo
