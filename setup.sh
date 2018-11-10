044c34e1de065ccff1e2d1a91281d7aaf5cf4992
mikefab <mikefabrikant@gmail.com>
2018-11-10 17:07:04 -0500

    Don't run docker, and execute bash in mb kepler demo too

--- setup.sh
+++ setup.sh
@@ -6,15 +6,14 @@ echo Adding magicbox-kepler-demo repository
 cd auth/src/components || exit echo Sorry, something went wrong. Please try again.
 git clone git@github.com:unicef/magicbox-kepler-demo.git
 cd magicbox-kepler-demo
-cp ./client/.env-sample ./client/.env
-cp ./server/config-sample.json ./server/config.json
-cp ./client/config-sample.js ./client/config.js
+git checkout dev
+bash setup.sh
 cd ../../../.. || exit echo Sorry, something went wrong. Please try again.
 
 echo Creating server
 
 mv ./auth/src/components/magicbox-kepler-demo/server .
 
-echo Running docker-compose
-
-docker-compose up
+# echo Running docker-compose
+#
+# docker-compose up
