f31049db0b90a2f0fc8eb5b44aae8dc19513969c
mikefab <mikefabrikant@gmail.com>
2018-11-15 22:29:27 -0500

    Get right repo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo Adding magicbox-kepler-demo repository
 cp ./auth/.env-sample ./auth/.env
 mkdir ./auth/src/components
 cd auth/src/components || exit echo Sorry, something went wrong. Please try again.
-git clone https://github.com/unicef/magicbox-msal-graph-service.git
+git clone https://github.com/unicef/magicbox-kepler-demo.git
 cd magicbox-kepler-demo
 git checkout dev
 bash setup.sh
