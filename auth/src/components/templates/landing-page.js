d82436cef2bf7bf16ba249be1a4d9a4508d6ade8
mikefab <mikefabrikant@gmail.com>
2019-05-23 20:01:46 -0400

    Update landing page content

--- auth/src/components/templates/landing-page.js
+++ auth/src/components/templates/landing-page.js
@@ -14,29 +14,27 @@ const LandingPage = (props) => {
     </div>
     </div>
     <div className="App-container">
-      <div className="App-container-div-1">
-        <div className="App-sub-container">
-        <h1> Magicbox, the UNICEF Innovation Big Data platform, is the work of data scientists, software engineers, designers, and researchers
-        in NYHQ and Programme Offices, along with academic and private partnerships.</h1>
-        </div>
+    <div className="App-container-div-1">
+      <div className="App-sub-container">
+      <h4>Magicbox maps</h4>
+        <p className="align-left">
+          The mapping application behind this login is just a value proposition of Magicbox.
+        </p>
       </div>
+    </div>
       <div className="App-container-div-2">
         <div className="App-sub-container">
-        <h4>Magicbox maps</h4>
-          <p className="align-left">Use our mapping tool to generate insights for allocating resources, infrastructure planning, and emergency preparedness and response.</p>
-        </div>
-      </div>
-      <div className="App-container-div-2">
-        <div className="App-sub-container">
-        <h4>Why Big Data?</h4>
-        <p>Data captures many different parts of human behavior, mobility, and environmental patterns. We can use data to shape responses to disasters, epidemics, and other challenges, by telling those involved in the response:</p>
-          <ul>
-                <li>Where to focus their limited resources;</li>
-                <li>How people who are most at risk are thinking about a threat;</li>
-                <li>What information to provide to affected populations;</li>
-                <li>and ways to proactively inform vital work to protect vulnerable children.</li>
-          </ul>
-        <p>It also allows us to link this information to governments and other partners for real-time situational awareness and problem-solving.</p>
+        <h4>Magicbox</h4>
+        <h3>
+        Magicbox is about preparing UNICEF to do data science, machine learning, and artificial intelligence. It includes a platform and a team of scientists working with country offices on projects like:
+
+            <ul>
+                <li>models for forecasting an epidemic</li>
+                <li>mapping all schools in the world</li>
+            </ul>
+          Browse <a href='https://www.unicef.org/innovation/Magicbox'>here</a> for more information on Magicbox.
+          </h3>
+
         </div>
       </div>
     </div>
